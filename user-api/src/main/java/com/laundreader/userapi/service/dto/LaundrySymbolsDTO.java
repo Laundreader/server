58899bad7035256d21cd1815736857925a9bffad
chaewon12 <djacltk@naver.com>
2025-08-09 14:05:23 +0900

    fix: LaundrySymbolsDTOnull safe 처리

--- user-api/src/main/java/com/laundreader/userapi/service/dto/LaundrySymbolsDTO.java
+++ user-api/src/main/java/com/laundreader/userapi/service/dto/LaundrySymbolsDTO.java
@@ -1,23 +1,34 @@
 package com.laundreader.userapi.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LaundrySymbolsDTO {
-    private List<LaundrySymbolDetailDTO> waterWashing;
-    private List<LaundrySymbolDetailDTO> bleaching;
-    private List<LaundrySymbolDetailDTO> ironing;
-    private List<LaundrySymbolDetailDTO> dryCleaning;
-    private List<LaundrySymbolDetailDTO> wetCleaning;
-    private List<LaundrySymbolDetailDTO> wringing;
-    private List<LaundrySymbolDetailDTO> naturalDrying;
-    private List<LaundrySymbolDetailDTO> tumbleDrying;
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> waterWashing = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> bleaching = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> ironing = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> dryCleaning = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> wetCleaning = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> wringing = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> naturalDrying = new ArrayList<>();
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
+    private List<LaundrySymbolDetailDTO> tumbleDrying = new ArrayList<>();
 
     @Getter
     public static class LaundrySymbolDetailDTO {

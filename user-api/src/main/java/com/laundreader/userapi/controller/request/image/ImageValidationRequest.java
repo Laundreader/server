0dd2caf725fa52d08b6949d17f68827643c02cea
chaewon12 <djacltk@naver.com>
2025-08-17 23:32:44 +0900

    refactor: 사진 request 형식 수정

--- user-api/src/main/java/com/laundreader/userapi/controller/request/image/ImageValidationRequest.java
+++ user-api/src/main/java/com/laundreader/userapi/controller/request/image/ImageValidationRequest.java
@@ -2,29 +2,30 @@ package com.laundreader.userapi.controller.request.image;
 
 import com.laundreader.userapi.service.dto.ImageDTO;
 import com.laundreader.userapi.service.type.ImageType;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ImageValidationRequest {
-    @NotNull(message = "값이 null 일 수 없습니다.")
-    private Image image;
+	@NotNull(message = "값이 null 일 수 없습니다.")
+	private ImageType type;
+	@NotNull(message = "값이 null 일 수 없습니다.")
+	private Image image;
 
-    @Getter
-    public static class Image{
-        private ImageType type;
-        @NotBlank(message = "빈 문자열 입니다.")
-        private String format;
-        @NotBlank(message = "빈 문자열 입니다.")
-        private String data;
-    }
+	public ImageDTO toImageDTO() {
+		return ImageDTO.builder()
+			.format(image.format)
+			.data(image.data)
+			.build();
+	}
 
-    public ImageDTO toImageDTO(){
-        return ImageDTO.builder()
-                .type(image.type)
-                .format(image.format)
-                .data(image.data)
-                .build();
-    }
+	@Getter
+	public static class Image {
+		@NotBlank(message = "빈 문자열 입니다.")
+		private String format;
+		@NotBlank(message = "빈 문자열 입니다.")
+		private String data;
+	}
 }
--- user-api/src/main/java/com/laundreader/userapi/service/ImageService.java
+++ user-api/src/main/java/com/laundreader/userapi/service/ImageService.java
@@ -1,21 +1,24 @@
 package com.laundreader.userapi.service;
 
+import org.springframework.stereotype.Service;
+
 import com.laundreader.external.clova.service.ClovaStudioService;
 import com.laundreader.userapi.service.dto.ImageDTO;
 import com.laundreader.userapi.service.response.image.ImageValidationResponseDTO;
+import com.laundreader.userapi.service.type.ImageType;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ImageService {
 
-    private final ClovaStudioService clovaStudioService;
+	private final ClovaStudioService clovaStudioService;
 
-    public ImageValidationResponseDTO validImageType(ImageDTO image) {
-        boolean result =  clovaStudioService.imageAnalysis(image.getType().getValue(), image.getData());
-        return new ImageValidationResponseDTO(result);
-    }
+	public ImageValidationResponseDTO validImageType(ImageType type, ImageDTO image) {
+		boolean result = clovaStudioService.imageAnalysis(type.getValue(), image.getData());
+		return new ImageValidationResponseDTO(result);
+	}
 }

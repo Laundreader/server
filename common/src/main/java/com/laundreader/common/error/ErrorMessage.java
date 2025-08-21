7279c74dce3627e36f7d69279cf854acd0586e5a
chaewon12 <djacltk@naver.com>
2025-08-21 22:09:49 +0900

    feat: 현재 날씨 API

--- common/src/main/java/com/laundreader/common/error/ErrorMessage.java
+++ common/src/main/java/com/laundreader/common/error/ErrorMessage.java
@@ -1,12 +1,15 @@
 package com.laundreader.common.error;
 
 public class ErrorMessage {
-    public static final String INTERNAL_ERROR = "internal error...";
+	public static final String INTERNAL_ERROR = "internal error...";
 
-    public static final String LAUNDRY_LABEL_NOT_RECOGNIZED =   "세탁 라벨을 인식할 수 없습니다.";
-    public static final String CLOTHING_NOT_RECOGNIZED =   "의류를 인식할 수 없습니다.";
+	public static final String LAUNDRY_LABEL_NOT_RECOGNIZED = "세탁 라벨을 인식할 수 없습니다.";
+	public static final String CLOTHING_NOT_RECOGNIZED = "의류를 인식할 수 없습니다.";
 
-    public static final String ORC_REQUEST_FAILED = "OCR 요청 실패";
-    public static final String CLOVA_STUDIO_REQUEST_FAILED = "CLOVA STUDIO 요청 실패";
-    public static final String CLOVA_STUDIO_RESPONSE_PARSING_FAILED = "CLOVA STUDIO 응답 파싱 실패";
+	public static final String ORC_REQUEST_FAILED = "OCR 요청 실패";
+	public static final String CLOVA_STUDIO_REQUEST_FAILED = "CLOVA STUDIO 요청 실패";
+	public static final String CLOVA_STUDIO_RESPONSE_PARSING_FAILED = "CLOVA STUDIO 응답 파싱 실패";
+	public static final String OPEN_WEATHER_REQUEST_FAILED = "OpenWeather 요청 실패";
+	public static final String OPEN_WEATHER_RESPONSE_PARSING_FAILED = "OpenWeather 응답 파싱 실패";
+
 }

9466730773efb45a00cdaf4aa9825cf207b2f45a
chaewon12 <djacltk@naver.com>
2025-09-15 23:27:10 +0900

    #15 refactor: 공개 api path 변경

--- user-api/src/main/java/com/laundreader/userapi/controller/image/ImageController.java
+++ user-api/src/main/java/com/laundreader/userapi/controller/image/ImageController.java
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/user-api/image/")
+@RequestMapping("/public/image/")
 @RequiredArgsConstructor
 @Validated
 @Slf4j

53ac04133743cce47ae9ae0a82988d20be3bb2b8
chaewon12 <djacltk@naver.com>
2025-09-19 16:38:38 +0900

    #15 fix: 쿠키 도메인 지정

--- common/src/main/java/com/laundreader/common/util/CookieUtil.java
+++ common/src/main/java/com/laundreader/common/util/CookieUtil.java
@@ -7,6 +7,11 @@ public class CookieUtil {
 
 	public static ResponseCookie createCookie(String name, String value, long age) {
 		return ResponseCookie.from(name, value)
+			.domain(".laundreader.com") // 최상위 도메인
+			.httpOnly(true) // 브라우저에서 접근 불가
+			.secure(true) // https 환경에서만 쿠키가 발동
+			.path("/")
+			.sameSite(Cookie.SameSite.NONE.attributeValue())
 			.maxAge(age)
 			.path("/")
 			.secure(true) // https 환경에서만 쿠키가 발동

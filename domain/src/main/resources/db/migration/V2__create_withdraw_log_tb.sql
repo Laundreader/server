5479fcb3da1ee2e510855fb88d481e803c65d90b
chaewon12 <djacltk@naver.com>
2025-10-01 00:13:23 +0900

    #15 fix: sql 생성문 수정

--- domain/src/main/resources/db/migration/V2__create_withdraw_log_tb.sql
+++ domain/src/main/resources/db/migration/V2__create_withdraw_log_tb.sql
@@ -4,4 +4,4 @@ CREATE TABLE withdraw_log_tb (
     email VARCHAR(255) NOT NULL,
     reason VARCHAR(255),
     withdrawn_at DATETIME DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ;

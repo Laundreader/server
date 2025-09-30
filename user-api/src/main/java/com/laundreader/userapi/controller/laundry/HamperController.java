37e9532920896adc2e8b06846e2a245aebbdf4ae
chaewon12 <djacltk@naver.com>
2025-09-30 15:42:14 +0900

    #15 refactor: 빨래 바구니 솔루션 Request 변경

--- user-api/src/main/java/com/laundreader/userapi/controller/laundry/HamperController.java
+++ user-api/src/main/java/com/laundreader/userapi/controller/laundry/HamperController.java
@@ -6,15 +6,20 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laundreader.common.util.ApiUtils;
 import com.laundreader.domain.entity.user.User;
 import com.laundreader.userapi._core.security.auth.PrincipalDetails;
+import com.laundreader.userapi.request.laundry.HamperSolutionRequest;
 import com.laundreader.userapi.response.laundry.HamperGetResponse;
+import com.laundreader.userapi.response.laundry.HamperSolutionResponse;
 import com.laundreader.userapi.service.laundry.HamperService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -32,5 +37,16 @@ public class HamperController {
 		User user = principal.getUser();
 		HamperGetResponse hamper = hamperService.getHamper(user.getId());
 		return new ResponseEntity<>(ApiUtils.success(hamper.getHamper().isEmpty() ? null : hamper), HttpStatus.OK);
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/solution")
+	public ResponseEntity<ApiUtils.ApiResult<HamperSolutionResponse>> hamperSolution(
+		@Valid @RequestBody HamperSolutionRequest request,
+		@AuthenticationPrincipal PrincipalDetails principal
+	) {
+		User user = principal.getUser();
+		HamperSolutionResponse response = hamperService.getHamperSolution(request.getLaundryIds(), user.getId());
+		return new ResponseEntity<>(ApiUtils.success(response), HttpStatus.OK);
 	}
 }
--- user-api/src/main/java/com/laundreader/userapi/dto/laundry/HamperDTO.java
+++ user-api/src/main/java/com/laundreader/userapi/dto/laundry/HamperDTO.java
@@ -2,6 +2,8 @@ package com.laundreader.userapi.dto.laundry;
 
 import java.util.List;
 
+import com.laundreader.domain.entity.laundry.Laundry;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +11,14 @@ import lombok.Getter;
 @Getter
 public class HamperDTO {
 	List<LaundryDTO> laundry;
+
+	public static HamperDTO fromEntities(List<Laundry> laundries) {
+		List<LaundryDTO> dtos = laundries.stream()
+			.map(LaundryDTO::fromEntity)
+			.toList();
+
+		return HamperDTO.builder()
+			.laundry(dtos)
+			.build();
+	}
 }

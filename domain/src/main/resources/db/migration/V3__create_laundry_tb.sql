3810c14efe4773a45711f18658e3d001a015b429
chaewon12 <djacltk@naver.com>
2025-10-26 16:06:22 +0900

    #15 fix: Flyway 버전 통합

--- domain/src/main/resources/db/migration/V3__create_laundry_tb.sql
+++ domain/src/main/resources/db/migration/V3__create_laundry_tb.sql
@@ -4,9 +4,9 @@
 CREATE TABLE laundry_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     -- 소재 배열 JSON 저장
-    materials TEXT,
-    color VARCHAR(255),
-    clothes_type VARCHAR(255),
+    materials TEXT NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    clothes_type VARCHAR(255) NOT NULL
     has_print_or_trims BOOLEAN NOT NULL,
     -- 추가 정보 배열 JSON 저장
     additional_info TEXT,
@@ -18,6 +18,8 @@ CREATE TABLE laundry_tb (
     label_image_key VARCHAR(255),
     clothes_image_key VARCHAR(255),
     thumbnail_image_key VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     -- 사용자 FK
     user_id BIGINT NOT NULL,
     CONSTRAINT fk_laundry_user FOREIGN KEY (user_id) REFERENCES user_tb(id)
